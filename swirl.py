9ee9304582f5d31570e93c6fbb443a98679a79a2
Eric Naeseth <enaeseth@gmail.com>
2009-12-19 15:09:02 -0600

    Version bump.

--- swirl.py
+++ swirl.py
@@ -10,7 +10,7 @@ import functools
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, asynchronous as web_async
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 def make_asynchronous_decorator(io_loop):
     """
