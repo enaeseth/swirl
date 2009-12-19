4435e99e9cf040765ccc20b347494b36f0c01de7
Eric Naeseth <enaeseth@gmail.com>
2009-12-19 16:33:40 -0600

    Adding setup infrastructure.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='swirl',
+    description='Some sugar for asynchronous Tornado code',
+    version='0.1.0',
+    author='Eric Naeseth',
+    author_email='eric@naeseth.com',
+    url='http://code.naeseth.com/swirl/',
+    
+    py_modules=['swirl']
+)
